397498049ec3c1bc5fe31cf9d7bbcecaa8a0cf45
macksme <macksme@outlook.com>
2020-08-24 23:15:13 +0800

    revert to using string for subtotal, subtotaltax

--- models/product.go
+++ models/product.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"strconv"
 	"encoding/json"
 	"strings"
 )
@@ -17,25 +16,9 @@ type ProductItem struct {
 	SKU string             `json:"sku,omitempty"`
 	TaxClass string        `json:"tax_class,omitempty"`
 	Metadata []MetaItem    `json:"meta_data,omitempty"`
-	Subtotal float64       `json:"subtotal,omitempty"` //fd
-	SubtotalTax float64    `json:"subtotal_tax,omitempty"` //fd
-	Price float64          `json:"price,omitempty"` //fd
-}
-
-// MarshalJSON serializes a ProductItem struct to JSON encoded data
-func (c *ProductItem) MarshalJSON() ([]byte, error) {
-	type jsonObj ProductItem
-	return json.Marshal(&struct{
-		Price           string `json:"price,omitempty"`
-		Subtotal        string `json:"subtotal,omitempty"`
-		SubtotalTax     string `json:"subtotal_tax,omitempty"`
-		*jsonObj
-	}{
-		Price: strconv.FormatFloat(c.Price, 'f', -1, 64),
-		Subtotal: strconv.FormatFloat(c.Subtotal, 'f', -1, 64),
-		SubtotalTax: strconv.FormatFloat(c.SubtotalTax, 'f', -1, 64),
-		jsonObj:  (*jsonObj)(c),
-	})
+	Subtotal string        `json:"subtotal,omitempty"` //fd
+	SubtotalTax string     `json:"subtotal_tax,omitempty"` //fd
+	Price string           `json:"price,omitempty"` //fd
 }
 
 // UnmarshalJSON parses JSON encoded data to a ProductItem struct
@@ -46,21 +29,15 @@ func (c *ProductItem) UnmarshalJSON(data []byte) error {
 
 	type rObj ProductItem
 	aux := &struct{
-		Subtotal        string `json:"subtotal,omitempty"`
-		SubtotalTax     string `json:"subtotal_tax,omitempty"`
 		*rObj
 	}{
 		rObj: (*rObj)(c),
 	}
-	//aux.TaxableCost = &TaxableCost{}
 
-	var subtotalStr, subtotalTaxStr string
 	if err := json.Unmarshal(data, &aux); err != nil {
 		if strings.Contains(err.Error(), "cannot unmarshal string into Go struct") && strings.Contains(err.Error(), "[]models.MetaItem") {
 			aux2 := &struct{
 				Metadata        string `json:"meta_data,omitempty"`
-				Subtotal        string `json:"subtotal,omitempty"`
-				SubtotalTax     string `json:"subtotal_tax,omitempty"`
 				*rObj
 			}{
 				rObj: (*rObj)(c),
@@ -69,33 +46,9 @@ func (c *ProductItem) UnmarshalJSON(data []byte) error {
 			if err2 := json.Unmarshal(data, &aux2); err2 != nil {
 				return err2
 			}
-
-			subtotalStr = aux2.Subtotal
-			subtotalTaxStr = aux2.SubtotalTax
 		} else {
 			return err
 		}
-	} else {
-		subtotalStr = aux.Subtotal
-		subtotalTaxStr = aux.SubtotalTax
-	}
-
-	var errFloat error
-
-	/*
-	c.Price, errFloat = strconv.ParseFloat(aux.Price, 64)
-	if errFloat != nil {
-		return errFloat
-	}
-	*/
-
-	c.Subtotal, errFloat = strconv.ParseFloat(subtotalStr, 64)
-	if errFloat != nil {
-		return errFloat
-	}
-	c.SubtotalTax, errFloat = strconv.ParseFloat(subtotalTaxStr, 64)
-	if errFloat != nil {
-		return errFloat
 	}
 
 	return nil
